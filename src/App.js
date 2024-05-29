def715542a752f06efa340fde72457a7cca0fe0e
Rayl Xylem Regañon <raylxylem@gmail.com>
2024-05-29 20:36:10 +0800

    pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import PokemonProfile from './components/PokemonProfile';
+import PokemonList from './components/PokemonList';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemon, setPokemon] = useState();
+  const [page, setPage] = useState(1);
+
+  async function getPokemonList(){
+    await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${10 * page}&limit=10`).then((res)=>{
+      res.json().then((data)=>{
+        setPokemonList(data.results)
+      })
+    })
+  }
+
+  async function getPokemon(pokemonName){
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((res)=>{
+      res.json().then((data)=>{
+        setPokemon(data)
+      })
+    })
+  }
+
+  useEffect(()=>{
+    getPokemonList()
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PokemonList pokemonList={pokemonList} page={page} setPage={setPage} getPokemon={getPokemon}/>
+      {pokemon && 
+        <PokemonProfile pokemon={pokemon}/>
+      }
     </div>
   );
 }
